2a43153a1c4cb9177f683437ad21bfda5f913b40
baiyidjp <tztddong@dongjiangpengdeMac-mini.local>
2016-03-17 15:42:13 +0800

    1.0

--- JPTableViewDelegate/ViewController.m
+++ JPTableViewDelegate/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "JPTableViewDelegate.h"
 
 @interface ViewController ()
+
+@property(nonatomic,strong)JPTableViewDelegate *tableViewDelegate;
+
+@property(nonatomic,strong)UITableView *mainTableView;
 
 @end
 
@@ -17,6 +22,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *dataList = @[@"第一行",@"第二行",@"第三行",@"第四行"];
+    
+    self.tableViewDelegate = [JPTableViewDelegate createTableViewDelegateWithDataList:dataList selectBlock:^(NSIndexPath *indexPath) {
+        
+        NSLog(@"点击第%zd行",indexPath.row);
+    }];
+    
+    self.mainTableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    [self.view addSubview:self.mainTableView];
+    self.mainTableView.delegate = self.tableViewDelegate;
+    self.mainTableView.dataSource = self.tableViewDelegate;
+    
 }
 
 - (void)didReceiveMemoryWarning {
